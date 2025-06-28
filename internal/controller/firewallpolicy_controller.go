37d806099565bec73302e97aeac057c462b928a3
Vegard Engen <vegard@engen.priv.no>
2025-06-29 00:44:49 +0200

    Check for  nil ResourcesManaged

--- internal/controller/firewallpolicy_controller.go
+++ internal/controller/firewallpolicy_controller.go
@@ -125,8 +125,7 @@ func (r *FirewallPolicyReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 			}
 			log.Info("Running finalizer logic for FirewallPolicy", "name", firewallPolicy.Name)
 
-			log.Info(fmt.Sprintf("Deleting %+v", firewallPolicy))
-			if true {
+			if firewallPolicy.Status.ResourcesManaged != nil {
 				if len(firewallPolicy.Status.ResourcesManaged.UnifiFirewallPolicies) > 0 {
 					for i, UnifiFirewallPolicy := range firewallPolicy.Status.ResourcesManaged.UnifiFirewallPolicies {
 						log.Info(fmt.Sprintf("From: %s to: %s TcpIpv4: %s UdpIpv4: %s TcpIpv6: %s UdpIpv6: %s", UnifiFirewallPolicy.From, UnifiFirewallPolicy.To, UnifiFirewallPolicy.TcpIpv4ID, UnifiFirewallPolicy.UdpIpv4ID, UnifiFirewallPolicy.TcpIpv6ID, UnifiFirewallPolicy.UdpIpv6ID))
